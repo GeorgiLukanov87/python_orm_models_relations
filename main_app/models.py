fabcca803e972d510867cba4dc07e094b15bb0b8
Georgi St. Lukanov <goshiii87@gmail.com>
2024-05-17 15:53:28 +0300

    task 1

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(
+        max_length=40,
+    )
+
+
+class Book(models.Model):
+    title = models.CharField(
+        max_length=40,
+    )
+
+    price = models.DecimalField(
+        max_digits=5,
+        decimal_places=2,
+    )
+
+    # Owner
+    author = models.ForeignKey(
+        to=Author,
+        on_delete=models.CASCADE,
+    )
